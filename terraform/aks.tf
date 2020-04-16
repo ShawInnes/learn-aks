b024b4e62284b49111a86f5b0995c04922b68aad
Shaw Innes <shaw@immortal.net.au>
2020-04-16 21:53:34 +1000

    Add vnet to terraform

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -2,8 +2,19 @@
 resource "azurerm_resource_group" "aks" {
   name = var.RESOURCE_GROUP
   location = var.REGION_NAME
-
   tags = {
     environment = "Learn"
   }
 }
+
+resource "azurerm_virtual_network" "aks" {
+  name = var.VNET_NAME
+  resource_group_name = var.RESOURCE_GROUP
+  address_space = ["10.0.0.0/8"]
+  location = var.REGION_NAME
+  subnet {
+    address_prefix = "10.240.0.0/16"
+    name = var.SUBNET_NAME
+  }
+}
+
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,7 @@
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=2.5.0"
+
+  features {
+  }
 }
